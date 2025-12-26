c6b854ba8b4ca8888e7cdfc9599980b216db6f2a
srivaishnavi567-coder <srivaishnavi567@gmail.com>
2025-12-26 10:26:20 +0530

    Fix Python package structure

--- sify_langfuse_sdk/span.py
+++ sify_langfuse_sdk/span.py
@@ -23,6 +23,46 @@
 #         ) as obs:
 #             obs.update(metadata=metadata)
 
+
+# class TraceSpan:
+#     def __init__(self, root_span):
+#         self.root = root_span
+
+#     def generation(
+#         self,
+#         model: str,
+#         input,
+#         output,
+#         usage_details: dict | None = None,
+#         cost_details: dict | None = None,
+#     ):
+#         """
+#         Creates a Langfuse GENERATION observation.
+#         Fully compliant with Langfuse docs:
+#         - input
+#         - output
+#         - usage_details
+#         - cost_details
+#         """
+
+#         with self.root.start_as_current_observation(
+#             as_type="generation",
+#             name="model-generation",
+#             model=model,
+#             input=input,
+#         ) as generation:
+#             generation.update(
+#                 output=output,
+#                 usage_details=usage_details,
+#                 cost_details=cost_details,
+#             )
+
+#     def event(self, name: str, metadata: dict):
+#         with self.root.start_as_current_observation(
+#             as_type="span",
+#             name=name,
+#         ) as span:
+#             span.update(metadata=metadata)
 class TraceSpan:
     def __init__(self, root_span):
         self.root = root_span
