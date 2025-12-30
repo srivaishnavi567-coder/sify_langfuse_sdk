2306222938a012182e2d536d18e090235c0239f9
srivaishnavi567-coder <srivaishnavi567@gmail.com>
2025-12-30 09:59:57 +0530

    enabling disablingthe langfuse

--- sify_langfuse_sdk/__init__.py
+++ sify_langfuse_sdk/__init__.py
@@ -1,8 +1,5 @@
 from .trace import LangfuseTracer
 from .config import configure_langfuse
 
-__all__ = [
-    "LangfuseTracer",
-    "configure_langfuse",
-]
+__all__ = ["LangfuseTracer", "configure_langfuse"]
 
